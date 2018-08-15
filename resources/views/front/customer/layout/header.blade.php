c095b308c7c822aea911c7c36f0a8ee015858821
Andry Andrianaivo <aa@alternateeve.com>
2018-08-15 08:22:58 +0200

    page login et cliche

--- resources/views/front/customer/layout/header.blade.php
+++ resources/views/front/customer/layout/header.blade.php
@@ -17,12 +17,12 @@
         </a>
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
-                <li class="dropdown notifications-menu">
+                <!-- <li class="dropdown notifications-menu">
                     <a href="{!! URL::to('/admin/tickets') !!}">
                       <i class="fa fa-bell-o"></i>
                       <span class="label label-warning">{!! count_tickets() !!}</span>
                     </a>
-                </li>
+                </li> -->
                 <!-- User Account: style can be found in dropdown.less -->
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
