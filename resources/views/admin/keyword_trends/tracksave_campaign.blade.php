0bc41d0f8e58e741829874dc1b6378e8a7e35d9a
Feno <fenoheriniainat@gmail.com>
2018-09-06 00:34:11 +0200

    Enlever researh month dans track and save

--- resources/views/admin/keyword_trends/tracksave_campaign.blade.php
+++ resources/views/admin/keyword_trends/tracksave_campaign.blade.php
@@ -152,7 +152,7 @@
                                   </div>
                               </div>
                               <div class="col-lg-6">
-                                  <div class="form-group flex_bottle">
+                                  <div class="form-group flex_bottle hidden">
                                       {!! Form::label('monthly_searches', 'Include monthly research', ['class' => 'col-sm-6 control-label']) !!}
                                       <div class="col-sm-6">
                                           {!! Form::checkbox('monthly_searches', null,  $campaign->monthly_searches ) !!}
