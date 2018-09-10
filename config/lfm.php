a7d04cb9862fbfb0e6064c6b07f9dc71e0d71e46
Feno <fenoheriniainat@gmail.com>
2018-09-10 09:05:16 +0200

    Mise à jour du lundi

--- config/lfm.php
+++ config/lfm.php
@@ -121,9 +121,16 @@ return [
         'image/pjpeg',
         'image/png',
         'image/gif',
-        'image/svg+xml',
         'application/pdf',
         'text/plain',
+        'image/svg+xml',
+        'application/pdf',
+        'application/docx',
+        'image/vnd.microsoft.icon',
+        'image/x-icon',
+        'application/vnd.ms-excel',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'application/docx'
     ],
 
     /*
